e6416db105bb28c42261cc45f129f17429aaff06
xq20168504 <59816445+xq20168504@users.noreply.github.com>
2020-01-13 13:49:16 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,17 +3,17 @@ var config = {
     // 句子的长度可以任意， 你可以写十句话， 二十句话都可以
     // 每句话尽量不要超过15个字,不然展示效果可能不太好
     texts: [
-        "送给我",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
-        "心爱的小可爱",  // 同上...
-        "今天是你的生日",
-        "这是我们在一起的",
-        "第三个生日了哦",
-        "去年的生日",
-        "还记得在王婆串串吃的呢",
-        "今年要吃好的喽哦",
-        "要把我家可爱猪猪喂饱饱",
-        "然后抱走",
-        "YAMI~~",
+        "People who can simplify complex problems are talents,",      //这里,每句话结尾的最后一个逗号必须是英文的哦!! 很重要哦!!
+        "and it is stupid to be able to complicate simple problems.",  // 同上...
+        "There is less natural suffering and less in the body;",
+        "less natural disasters in the mouth;",
+        "fewer natural diseases in the abdomen;",
+        "less desire in nature.",
+        "Today you do things who will not do,",
+        "tomorrow you will do things who can’t do.",
+        "I will greet this day with love in my heart.",
+        "Everything will be ok in the end, if it's not ok, it's not the end.",
+        "So what if we fall down? At least we are still young.",
     ],
     /**
      * imgs 可以不填, 但是如果要填写的话必须遵循下面的格式
@@ -30,13 +30,13 @@ var config = {
     },
     // 按钮文字描述, 以下是默认的按钮文字，英文的，您可以改成你喜欢的文字
     desc: {
-        turn_on: "开始",
-        play: "音乐",
-        bannar_coming: "颜色",
-        balloons_flying: "好像少点东西",
-        cake_fadein: "蛋糕？",
-        light_candle: "蜡烛？",
-        wish_message: "生日快乐",
+        turn_on: "Start",
+        play: "Music",
+        bannar_coming: "Color",
+        balloons_flying: "There is something missing...",
+        cake_fadein: "Cake？",
+        light_candle: "Candle？",
+        wish_message: "Always love you all",
         story: "A MESSAGE FOR YOU",
     }
 };
